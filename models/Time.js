fc132efc2eb208698a000499cc5f64eeb9b05a2e
Genevieve Ferrari <genferrari@gmail.com>
2023-01-17 20:44:03 -0800

    added basic files to models & seeds folder

---
+++ models/Time.js
@@ -0,0 +1,28 @@
+const { Model, DataTypes } = require("sequelize");
+
+const sequelize = require("../config/connection.js");
+
+class Time extends Model {}
+
+Time.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    time_name: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "time",
+  }
+);
+
+module.exports = Tag;
