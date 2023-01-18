048326bb38c24727258bf993602de6e0d2456906
Genevieve Ferrari <genferrari@gmail.com>
2023-01-18 12:38:42 -0800

    corrected errors in models & updated seeds to reflect corrections

---
+++ models/index.js
@@ -0,0 +1,33 @@
+// import models
+const Product = require("./Product");
+const Category = require("./Category");
+const Time = require("./Time");
+const ProductTime = require("./ProductTime");
+
+// Products belongsTo Category
+Product.belongsTo(Category);
+
+// Categories have many Products
+Category.hasMany(Product);
+// Products belongToMany Time (through ProductTime)
+Product.belongsToMany(Time, {
+  through: {
+    model: ProductTime,
+    unique: false,
+  },
+});
+// Time belongToMany Products (through ProductTime)
+
+Time.belongsToMany(Product, {
+  through: {
+    model: ProductTime,
+    unique: false,
+  },
+});
+
+module.exports = {
+  Product,
+  Category,
+  Time,
+  ProductTime,
+};
