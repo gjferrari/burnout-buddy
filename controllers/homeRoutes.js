6936a178d7649b5a5c7affef96735f6f57ebf455
Mathew Chandler <Cascabelrock@gmail.com>
2023-01-17 21:04:44 -0800

    created homeroutes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,87 @@
+const router = require('express').Router();
+const { Category, Time, Product } = require('../models');
+
+// GET all models for homepage
+router.get('/', async (req, res) => {
+  try {
+    const dbCategoryData = await Category.findAll({
+      include: [
+        {
+          model: Category,
+          attributes: ['filename', 'description'],
+        },
+      ],
+    });
+
+    const categories = dbCategoryData.map((category) =>
+      category.get({ plain: true })
+    );
+
+    res.render('homepage', {
+      categories,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET one category
+router.get('/category/:id', async (req, res) => {
+  // If the user is not logged in, redirect the user to the login page
+  if (!req.session.loggedIn) {
+    res.redirect('/login');
+  } else {
+    // If the user is logged in, allow them to view the gallery
+    try {
+      const dbCategoryData = await Category.findByPk(req.params.id, {
+        include: [
+          {
+            model: Product,
+            attributes: [
+                // todo: need attributes for product
+              '',
+            ],
+          },
+        ],
+      });
+      const category = dbCategoryData.get({ plain: true });
+      res.render('category', { category, loggedIn: req.session.loggedIn });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  }
+});
+
+// GET one product
+router.get('/product/:id', async (req, res) => {
+  // If the user is not logged in, redirect the user to the login page
+  if (!req.session.loggedIn) {
+    res.redirect('/login');
+  } else {
+    // If the user is logged in, allow them to view the product
+    try {
+      const dbProductData = await Painting.findByPk(req.params.id);
+
+      const product = dbProductData.get({ plain: true });
+
+      res.render('product', { product, loggedIn: req.session.loggedIn });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
