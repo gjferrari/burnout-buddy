8ffdc2db2cd69ffa2d98e80b0320bf1f20bc6971
Mathew Chandler <Cascabelrock@gmail.com>
2023-01-20 15:43:38 -0800

    imported time routes and category routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
-
+const productRoutes = require("./productRoutes")
 const categoryRoutes = require('./categoryRoutes');
 const userRoutes = require('./userRoutes');
+const timeRoutes = require("./timeRoutes")
 
-router.use('/categoryRoutes', userRoutes);
+router.use('/categoryRoutes', categoryRoutes);
+router.use('/productRoutes', productRoutes);
+router.use('/timeRoutes', timeRoutes);
 router.use('/User', userRoutes);
 module.exports = router;
