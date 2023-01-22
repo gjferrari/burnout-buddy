b9e687761ed69e9cc03612b92e1020dc0f055069
Genevieve Ferrari <genferrari@gmail.com>
2023-01-21 17:47:39 -0800

    added get by id

--- controllers/api/productRoutes.js
+++ controllers/api/productRoutes.js
@@ -14,7 +14,7 @@ router.get("/", async (req, res) => {
       {
         model: Time,
         // attributes: ["id", "time_amount"],
-        // you only need to use attributes to exclude things but 
+        // you only need to use attributes to exclude things but
         //since Time only has id and time_amount you can just refer to the Time model
       },
       {
@@ -32,10 +32,28 @@ router.get("/", async (req, res) => {
 });
 
 // get one product
-router.get("/:id", (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Time data
+
+router.get("/:id", async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Time }, { model: Category }],
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: "No product found with this id!" });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
+// router.get("/:id", (req, res) => {
+//   // find a single product by its `id`
+//   // be sure to include its associated Category and Time data
+// });
 
 // create new product
 router.post("/", (req, res) => {
