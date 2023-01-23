19bfd4b047db05c5a5846ed393f528096773c6b4
Wambaugh <marwambaugh@gmail.com>
2023-01-22 16:18:16 -0800

    addedFakeUsers

--- seeds/user_seeds/user-seeds.js
+++ seeds/user_seeds/user-seeds.js
@@ -1,6 +1,44 @@
 const { User } = require("../models");
 
-const userData = [];
+//model has user_id but I'm not sure we need it for userData= "auto increment:true" 
+
+const userData = [
+    {
+        name:"gFerrari",
+        email:"gFerrari@gmail.com",
+        password:"password1",
+    },
+    {
+        name:"cBain",
+        email:"cBain@gmail.com",
+        password:"password2",
+    },
+    {
+        name:"Ncohen",
+        email:"Ncohen@gmail.com",
+        password:"password3",
+    },
+    {
+        name:"MChandler",
+        email:"Mchandler@gmail.com",
+        password:"password4",
+    },
+    {
+        name:"FBaggins",
+        email:"FrodoBaggins@gmail.com",
+        password:"password5",
+    },
+    {
+        name:"RigbyW",
+        email:"RigbyWilson@gmail.com",
+        password:"password6",
+    },
+    {
+        name:"EllaC",
+        email:"EllaCohen@gmail.com",
+        password:"password7",
+    },
+];
 
 const seedUser = () => User.bulkCreate(userData);
 
